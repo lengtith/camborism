b003893a699c2cb5d6692e80336156673e5ed79f
lengtith <sotith1998@gmail.com>
2025-01-02 09:54:11 +0700

    camborism

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: true,
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.spec.ts",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  }
+});
