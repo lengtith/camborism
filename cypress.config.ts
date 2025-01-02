80e70bc24bef8dcdae3231aa6b34350060f518aa
lengtith <sotith1998@gmail.com>
2025-01-02 10:28:32 +0700

    camborism

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import fs from "fs";
 
 export default defineConfig({
   video: true,
@@ -6,7 +7,21 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.ts",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on(
+        "after:spec",
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === "failed")
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        }
+      );
     }
   }
 });
